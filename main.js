1f5fc6ac90c920d63469daf7735f8f439e46a0aa
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2024-11-23 17:52:08 +0530

    api works properly

---
+++ main.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import banner from './routes/banner.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Use the banner router
+app.use('/banner', banner);
+
+app.get('/', (req, res) => {
+    res.send('Test route works!');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
