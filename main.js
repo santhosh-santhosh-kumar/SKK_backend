598e3bf69cf2f9c42bdfcef03aff5432e7b4dc7d
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2024-11-26 18:01:28 +0530

    cors error fix

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ import bannerShorts from './routes/bannerShorts.route.js';
 const app = express();
 const PORT = process.env.PORT || 3000
 app.use(express.json())
-app.use(cors());
+app.use(
+    cors({
+      origin:"http://localhost:5175/"
+    })
+  );
 connectDB()
 
 // Use the banner router
