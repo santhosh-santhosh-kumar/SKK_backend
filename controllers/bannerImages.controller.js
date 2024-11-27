afdb3181c2eb3d01c4ca483631d0f584970e129e
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2024-11-27 13:07:03 +0530

    commit

--- controllers/bannerImages.controller.js
+++ controllers/bannerImages.controller.js
@@ -43,7 +43,7 @@ export const imageCreate=async(req,res,next)=>{
       fileName: `${file.filename}`,
       contentType: file.mimetype,
       path: file.path,
-      url:`https://skk-backend.onrender.com/BannerImages/${file.filename}`
+      url:`https://skk-backend.onrender.com/bannerImages/${file.filename}`
     }));
     await bannerImages.insertMany(files);
 
