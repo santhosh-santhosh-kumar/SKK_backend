1ecb16d9f581b77816362f90719ffea09480f854
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2024-11-27 11:29:27 +0530

    dir name error fix

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ import connectDB from './database/db.js';
 import bannerimages from "./routes/bannerImages.route.js";
 import cors from "cors";
 import bannerShorts from './routes/bannerShorts.route.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors());
-app.use(express.static('uploads'))
+app.use(express.static(path.join(__dirname,'Banner_Images')))
 connectDB()
 
 // Use the banner router
