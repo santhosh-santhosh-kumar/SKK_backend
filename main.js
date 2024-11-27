69a58d594d395f48e8e593804be08a9962646afc
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2024-11-27 11:54:01 +0530

    chage uploads dir

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors());
-app.use('/Banner_Images',express.static(path.join(__dirname,'Banner_Images')))
+app.use('/uploads',express.static(path.join(__dirname,'uploads')))
 connectDB()
 
 // Use the banner router
