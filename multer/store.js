30a2d4cab03cbcd0261199f4565c7b891cca913f
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2024-11-27 13:01:40 +0530

    fix

--- multer/store.js
+++ multer/store.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const storage=multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,path.join(__dirname, 'bannerImages'))
+        cb(null,'bannerImages')
     },
     filename:(req,file,cb)=>{
     cb(null,file.fieldname+'_'+Date.now()+path.extname(file.originalname))
