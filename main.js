401ba361426bf30fb102c18b453ef5a675c911f5
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2024-11-28 11:26:27 +0530

    static files upload

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors());
 app.use('/bannerImages',express.static('bannerImages'))
+app.use('/homeAboutImages',express.static('homeAboutImages'))
 connectDB()
 
 // Use the banner router
